9541e02c8b170ac1f7794558922be87815f01abf
Shubha-Tripathi <shubha.trist@gmail.com>
2022-12-22 21:17:57 -0800

    Edited code for a better UX

--- Day 3 - Indian_Matchmaker_Game.py
+++ Day 3 - Indian_Matchmaker_Game.py
@@ -18,10 +18,10 @@ print('''
 print("Welcome to Seema Auntie's Treasure Island! Where we don't know if you will find a treasure but you will certainly learn how to 'settle'!")
 print("You mission is to find your Indian soulmate :)")
 
-coffee = input("You are at a cross road. On one end you see a Starbucks and the other you see a Dunkin Donuts. Where are you going to get your coffee? Type 'S' or 'D'. ").lower()
+coffee = input("You are at a cross road. On one end you see a Starbucks and the other you see a Dunkin Donuts. Where are you going to get your coffee? Type 'S' or 'D'.\n").lower()
 
 if coffee == "s":
-    activity = input("In your free time, what do you envision yourself doing? Doing yoga, mixing cocktails, traveling, or playing animal crossing? Type 'Y', 'C', 'T', or 'A'. ").lower()
+    activity = input("In your free time, what do you envision yourself doing? Doing yoga, mixing cocktails, traveling, or playing animal crossing? Type 'Y', 'C', 'T', or 'A'.\n").lower()
     if activity == "y":
         print("Congrats! You are matched with Nadia. Enjoy your time with the sweet bubbly queen, who will probably start hounding you for kids in the next 6 months!")
     elif activity == "c":
@@ -29,13 +29,13 @@ if coffee == "s":
     elif activity == "t":
         print("Congrats! You are matched with Viral. Our queen has high aspirations and even higher expectations. Be ready to confront your mommy issues, because she has the knack of bringing them out!")
     else:
-        shubha_choice = input("You were matched with your truly! But first things first, do you like dogs and is your name Gavin? Type 'Y' or 'N'. ").lower()
+        shubha_choice = input("You were matched with your truly! But first things first, do you like dogs and is your name Gavin? Type 'Y' or 'N'.\n").lower()
         if shubha_choice == "y":
             print("YOU HAVE WON THE PRIZE!! Get ready for a snack-loving, low-key grouchy, and beautiful partner <3.")
         else:
             print("Shubha says, 'Thank you for your interest, but we will be moving forward with other qualified candidates.'")
 else: 
-    boring = input("Do you enjoy going out with your friends or staying at home and reading? Type 'O' or 'R'. ").lower()
+    boring = input("Do you enjoy going out with your friends or staying at home and reading? Type 'O' or 'R'.\n").lower()
     if boring == "o":
         print("Congrats! You are matched with Akshay. Who can beat a balding head, unearned self-confidence, and a million chicken stories?")
     else:
